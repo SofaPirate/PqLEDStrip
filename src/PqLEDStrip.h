0284fc1a36b6b03a3a38540a6d318cf162deee17
Thomas O Fredericks <thomas@fredericks.info>
2025-08-04 21:38:27 -0400

    Combined palette with the strip

--- src/PqLEDStrip.h
+++ src/PqLEDStrip.h
@@ -34,116 +34,6 @@ namespace pq
 
     // --------------------------------------------------------
 
-    class LEDStripPalette
-    {
-
-    private:
-        enum class PaletteType
-        {
-            NONE,
-            PALETTE16,
-            PALETTE32,
-            PALETTE256,
-            PROGMEM16,
-            HSV16,
-            HSV32,
-            HSV256
-        };
-
-        PaletteType _type = PaletteType::NONE;
-        TBlendType _blend = LINEARBLEND;
-
-        const CRGBPalette16 *_palette16;
-        const CRGBPalette32 *_palette32;
-        const CRGBPalette256 *_palette256;
-        const TProgmemRGBPalette16 *_progmemPalette16;
-        const CHSVPalette16 *_hsvPalette16;
-        const CHSVPalette32 *_hsvPalette32;
-        const CHSVPalette256 *_hsvPalette256;
-
-    public:
-        // Setters
-        void set(const CRGBPalette16 &p, TBlendType blend = LINEARBLEND)
-        {
-            _palette16 = &p;
-            _blend = blend;
-            _type = PaletteType::PALETTE16;
-        }
-
-        void set(const CRGBPalette32 &p, TBlendType blend = LINEARBLEND)
-        {
-            _palette32 = &p;
-            _blend = blend;
-            _type = PaletteType::PALETTE32;
-        }
-
-        void set(const CRGBPalette256 &p, TBlendType blend = LINEARBLEND)
-        {
-            _palette256 = &p;
-            _blend = blend;
-            _type = PaletteType::PALETTE256;
-        }
-
-        void set(const TProgmemRGBPalette16 &p, TBlendType blend = LINEARBLEND)
-        {
-            _progmemPalette16 = &p;
-            _blend = blend;
-            _type = PaletteType::PROGMEM16;
-        }
-
-        void set(const CHSVPalette16 &p, TBlendType blend = LINEARBLEND)
-        {
-            _hsvPalette16 = &p;
-            _blend = blend;
-            _type = PaletteType::HSV16;
-        }
-
-        void set(const CHSVPalette32 &p, TBlendType blend = LINEARBLEND)
-        {
-            _hsvPalette32 = &p;
-            _blend = blend;
-            _type = PaletteType::HSV32;
-        }
-
-        void set(const CHSVPalette256 &p, TBlendType blend = LINEARBLEND)
-        {
-            _hsvPalette256 = &p;
-            _blend = blend;
-            _type = PaletteType::HSV256;
-        }
-
-        /// Returns a color from the current palette using a normalized float in [0,1].
-        CRGB getColor(float value) const
-        {
-            uint8_t frac8 = static_cast<uint8_t>(constrain(value, 0.0f, 1.0f) * 255.0f);
-
-            switch (_type)
-            {
-            case PaletteType::PALETTE16:
-                return ColorFromPalette(*_palette16, frac8, 255, _blend);
-            case PaletteType::PALETTE32:
-                return ColorFromPalette(*_palette32, frac8, 255, _blend);
-            case PaletteType::PALETTE256:
-                return ColorFromPalette(*_palette256, frac8, 255, _blend);
-            case PaletteType::PROGMEM16:
-                return ColorFromPalette(*_progmemPalette16, frac8, 255, _blend);
-            case PaletteType::HSV16:
-                return ColorFromPalette(*_hsvPalette16, frac8, 255, _blend);
-            case PaletteType::HSV32:
-                return ColorFromPalette(*_hsvPalette32, frac8, 255, _blend);
-            case PaletteType::HSV256:
-                return ColorFromPalette(*_hsvPalette256, frac8, 255, _blend);
-            case PaletteType::NONE:
-            default:
-                return CRGB(frac8, frac8, frac8);
-            }
-        }
-        /*
-            PaletteType type() const { return _type; }
-            TBlendType blend() const { return _blend; }
-            */
-    };
-
     // =================================================
 
     template <uint32_t PIN, EOrder ORDER, uint32_t COUNT>
@@ -159,8 +49,29 @@ namespace pq
         float _value;
         float _normalizeStepValue;
 
-    public:
-        LEDStripPalette palette;
+    private:
+        enum class PaletteType
+        {
+            NONE,
+            PALETTE16,
+            PALETTE32,
+            PALETTE256,
+            PROGMEM16,
+            HSV16,
+            HSV32,
+            HSV256
+        };
+
+        PaletteType _paletteType = PaletteType::NONE;
+        TBlendType _blend = LINEARBLEND;
+
+        const CRGBPalette16 *_palette16;
+        const CRGBPalette32 *_palette32;
+        const CRGBPalette256 *_palette256;
+        const TProgmemRGBPalette16 *_progmemPalette16;
+        const CHSVPalette16 *_hsvPalette16;
+        const CHSVPalette32 *_hsvPalette32;
+        const CHSVPalette256 *_hsvPalette256;
 
     protected:
         void begin() override
@@ -174,89 +85,196 @@ namespace pq
             if (_needToShow)
             {
                 _needToShow = false;
-                //_pixels[0] = CRGB(255,0,0);
-                //_pixels[1] = CRGB(0,255,0);
                 controller->showLeds(255);
             }
         }
 
-
-    public:
-        /// Constructor.
-        LEDStripWS281X() : Unit()
-        {
-            // engine.addAttachment(&_fastLedSingletonAttachment);
-            // _value = constrain(initialValue, 0, 1);
-            if ( COUNT > 1 ) {
-                _normalizeStepValue = 1.0/(float(COUNT-1));
-            } else {
+        private:
+           void init() {
+            if (COUNT > 1)
+            {
+                _normalizeStepValue = 1.0 / (float(COUNT - 1));
+            }
+            else
+            {
                 _normalizeStepValue = 0;
             }
-            
+        
+    }
+
+    public:
+
+ 
+        /// Constructor.
+        LEDStripWS281X()
+        {
+            init();
+        }
+
+        void unsetPalette() {
+            _paletteType = PaletteType::NONE;
+        }
+
+        void setPalette(const CRGBPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            _palette16 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::PALETTE16;
+        }
+
+        void setPalette(const CRGBPalette32 &p, TBlendType blend = LINEARBLEND)
+        {
+            _palette32 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::PALETTE32;
+        }
+
+        void setPalette(const CRGBPalette256 &p, TBlendType blend = LINEARBLEND)
+        {
+            _palette256 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::PALETTE256;
+        }
+
+        void setPalette(const TProgmemRGBPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            _progmemPalette16 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::PROGMEM16;
+        }
+
+        void setPalette(const CHSVPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            _hsvPalette16 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::HSV16;
+        }
+
+        void setPalette(const CHSVPalette32 &p, TBlendType blend = LINEARBLEND)
+        {
+            _hsvPalette32 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::HSV32;
+        }
+
+        void setPalette(const CHSVPalette256 &p, TBlendType blend = LINEARBLEND)
+        {
+            _hsvPalette256 = &p;
+            _blend = blend;
+            _paletteType = PaletteType::HSV256;
+        }
+
+        LEDStripWS281X(const CRGBPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const CRGBPalette32 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const CRGBPalette256 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const TProgmemRGBPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const CHSVPalette16 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const CHSVPalette32 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+
+        LEDStripWS281X(const CHSVPalette256 &p, TBlendType blend = LINEARBLEND)
+        {
+            init();
+            setPalette(p, blend);
+        }
+        /*
+                void setPalette(LEDStripPalette & palette) {
+                    _palette = & palette;
+                }
+
+                void unsetPalette() {
+                    _palette = NULL;
+                }
+        */
+        CRGB getColor(float value) const
+        {
+            uint8_t frac8 = floor(value * 255.0f);
+
+            switch (_paletteType)
+            {
+            case PaletteType::PALETTE16:
+                return ColorFromPalette(*_palette16, frac8, 255, _blend);
+            case PaletteType::PALETTE32:
+                return ColorFromPalette(*_palette32, frac8, 255, _blend);
+            case PaletteType::PALETTE256:
+                return ColorFromPalette(*_palette256, frac8, 255, _blend);
+            case PaletteType::PROGMEM16:
+                return ColorFromPalette(*_progmemPalette16, frac8, 255, _blend);
+                //Serial.println(frac8);
+                //return ColorFromPalette(RainbowColors_p,frac8, 255, _blend);
+                //return CRGB(frac8, 0, 0);
+            case PaletteType::HSV16:
+                return ColorFromPalette(*_hsvPalette16, frac8, 255, _blend);
+            case PaletteType::HSV32:
+                return ColorFromPalette(*_hsvPalette32, frac8, 255, _blend);
+            case PaletteType::HSV256:
+                return ColorFromPalette(*_hsvPalette256, frac8, 255, _blend);
+            case PaletteType::NONE:
+            default:
+                return CRGB(frac8, frac8, frac8);
+            }
         }
 
         void draw(AbstractMap &map)
         {
-
-           // float stepInterval = getNormalizedStepInterval(COUNT);
-            // if COUNT == 2, step = 0.5, so interated values are 0, 0.5 and 1
-            // if COUNT == 3, step = 0.33333,
-            /*for (float f =0; f <= 1.0; f += step) {
-
-            }
-            */
             for (int i = 0; i < COUNT; i++)
             {
-
-                _pixels[i] = palette.getColor(map.read(float(i) * _normalizeStepValue));
+                float value = map.read(float(i) * _normalizeStepValue);
+                CRGB color = getColor(value);
+                _pixels[i] = color;
+                /*
+                Serial.print(i); Serial.print(" ");
+                Serial.print(value); Serial.print(" ");
+                Serial.print(color.r); Serial.print(" ");
+                Serial.print(color.g); Serial.print(" ");
+                Serial.print(color.b); Serial.println();
+                */
             }
 
             _needToShow = true;
         }
-        /*
-                void applyWave(AbstractWave &wave, float period = 1.0)
-                {
-                    if (period > 0)
-                    {
-                        float step = period / float(COUNT);
-                        for (int i = 0; i < COUNT; i++)
-                        {
-                            _pixels[i] = palette.getColor(wave.getShiftedByTime(float(i) * step));
-                        }
-                    }
-                }
-                    */
 
-        /*
-                            PqStripWS281X & pixel(int pixelIndex) {
-                                _pixelIndex = constrain(pixelIndex,0,COUNT);
-                                return *this;
-                            }
-
-                            PqStripWS281X &  setRGBFromBlend(CRGB start, CRGB end) {
-                                uint8_t amount = (uint8_t)(_value * 255.0);
-                                _pixels[_pixelIndex] = CRGB::blend(start, end, amount);
-                                return *this;
-                            }
-
-
-                    void setPalette( CRGB alpha , CRGB beta ) {
-
-                    }
-
-                    void pixel(int index) {
-
-                    }
-                */
-        // virtual ~PqStripWS281X() {}
+        void setPixel(int index, CRGB color) {
+            _pixels[index] = color;
+            _needToShow = true;
+        }
 
         // PUT SHOULD BE A FORCED OVERRIDE
         float put(float value) override
         {
-            _value = constrain01(value);
+            _value = value;
 
-            CRGB color = palette.getColor(_value);
+            CRGB color = getColor(_value);
             fill_solid(_pixels, COUNT, color);
+
             _needToShow = true;
 
             return get();
@@ -264,12 +282,6 @@ namespace pq
 
         /// Returns value in [0, 1].
         float get() override { return _value; }
-
-        /// Returns value in [0, 1].
-        // virtual float get() { return _value; }
-
-        /// Maps value to new range.
-        // virtual float mapTo(float toLow, float toHigh) { return mapFrom01(get(), toLow, toHigh); }
     };
 }
 #endif
