b3b754a08866e04b979255543424cca5dad0c302
Sofian Audry <info@sofianaudry.com>
2025-08-18 15:10:27 -0400

    Added new bump and notch modes for LevelField example.

--- examples/platformio/esp32_ring_level/src/main.cpp
+++ examples/platformio/esp32_ring_level/src/main.cpp
@@ -20,14 +20,15 @@ Wave wave(SINE);
 
 // Potentiometers.
 AnalogIn potRampWidth(35); // controls ramp width
-AnalogIn potRampShift(32); // controls ramp shift
-AnalogIn potCenter(33); // controls center
+AnalogIn potRampShift(33); // controls ramp shift
+AnalogIn potCenter(32); // controls center
 
-// Switch for rising vs falling.
+// Switches for mode.
+DigitalIn switchBump(14, INTERNAL_PULLUP);
 DigitalIn switchRising(12, INTERNAL_PULLUP);
 
 // Switch for direct vs wave.
-DigitalIn switchWave(14, INTERNAL_PULLUP);
+DigitalIn switchWave(27, INTERNAL_PULLUP);
 
 // Metronome updating the strip.
 Metronome stripMetronome{0.05};
@@ -57,8 +58,13 @@ void step()
         // Update level field parameters.
         levelField.rampWidth(potRampWidth.mapTo(0, 0.5));
         levelField.rampShift(potRampShift);
+        levelField.bumpWidth(0.25);
+
         levelField.center(potCenter);
-        levelField.setRising(switchRising);
+        if (switchBump)
+            levelField.mode(switchRising ? LEVEL_BUMP : LEVEL_NOTCH);
+        else
+            levelField.mode(switchRising ? LEVEL_RISING : LEVEL_FALLING); // setRising(switchRising);
 
         // Draw field on the strip.
         strip.draw(levelField);
